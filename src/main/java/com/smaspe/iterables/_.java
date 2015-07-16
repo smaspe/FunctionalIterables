87bb5e04cee509a7654d42de99dd69809e2db0bd
njzk2 <masperos+github@gmail.com>
2015-07-15 23:19:14 -0400

    Use Java8 Lambdas

--- src/main/java/com/smaspe/iterables/_.java
+++ src/main/java/com/smaspe/iterables/_.java
@@ -8,27 +8,22 @@ import java.util.Iterator;
 public class _ {
 
     public static <T> Iterable<T> iter(final T[] array) {
-        return new Iterable<T>() {
+        return () -> new Iterator<T>() {
+            public int i = 0;
+
             @Override
-            public Iterator<T> iterator() {
-                return new Iterator<T>() {
-                    public int i = 0;
+            public boolean hasNext() {
+                return i < array.length;
+            }
 
-                    @Override
-                    public boolean hasNext() {
-                        return i < array.length;
-                    }
+            @Override
+            public T next() {
+                return array[i++];
+            }
 
-                    @Override
-                    public T next() {
-                        return array[i++];
-                    }
-
-                    @Override
-                    public void remove() {
-                        throw new UnsupportedOperationException();
-                    }
-                };
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException();
             }
         };
     }
